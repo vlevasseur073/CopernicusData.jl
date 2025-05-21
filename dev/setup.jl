95af06cbfb0693b9bee02da5e3519c3b91e1b7de
Documenter.jl <documenter@juliadocs.github.io>
2025-05-21 05:32:37 +0000

    build based on c097cdb

---
+++ dev/setup.jl
@@ -0,0 +1,6 @@
+using CopernicusData
+using Plots
+using Downloads
+
+const PRODUCT_PATH = "https://common.s3.sbg.perf.cloud.ovh.net/eoproducts"
+const OLCEFR="S03OLCEFR_20230506T015316_0180_B117_T883.zarr.zip"
