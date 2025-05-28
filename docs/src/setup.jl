e26f64393f414c5f3391e89c8d17e15d869038e1
Vincent Levasseur <vince.levasseur@protonmail.com>
2025-05-28 09:06:29 +0200

    Remove zarr product from doc resources

--- docs/src/setup.jl
+++ docs/src/setup.jl
@@ -4,3 +4,5 @@ using Downloads
 
 const PRODUCT_PATH = "https://common.s3.sbg.perf.cloud.ovh.net/eoproducts"
 const OLCEFR="S03OLCEFR_20230506T015316_0180_B117_T883.zarr.zip"
+const SLSFRP="S03SLSFRP_20200908T182648_0179_A298_S883.zarr.zip"
+const SLSLST="S03SLSLST_20191227T124111_0179_A109_T883.zarr.zip"
