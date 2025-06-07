9d35f5dbffea65d7eb3c5039a64e5eb67e8cd6ab
Vincent Levasseur <vince.levasseur@protonmail.com>
2025-06-07 09:08:52 +0200

    Reactivate commented tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,8 @@ using Test
 
 include("test_cloud_storage.jl")
 include("test_yaxtree.jl")
-# include("test_aws.jl")
-# include("test_EOTriggering.jl")
-# include("test_EOProducts.jl")
-# include("test_interpolation.jl")
-# include("test_ExampleProcessor.jl")
+include("test_aws.jl")
+include("test_EOTriggering.jl")
+include("test_EOProducts.jl")
+include("test_interpolation.jl")
+include("test_ExampleProcessor.jl")
