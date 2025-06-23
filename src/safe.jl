6cb1cffebbf6549c55b127e10562bfaaf23c8d0d
vlevasseur073 <vince.levasseur@protonmail.com>
2025-06-23 09:11:18 +0200

    Add Sentinel-3 Level-2 LST SAFE product handling

--- src/safe.jl
+++ src/safe.jl
@@ -6,5 +6,6 @@ const TYPE_TO_MAPPING = Dict(
     "OL_1_EFR" => joinpath(dirname(@__DIR__), "src", "mappings", "S03OLCEFR_mapping.json"),
     "OL_2_LFR" => joinpath(dirname(@__DIR__), "src", "mappings", "S03OLCLFR_mapping.json"),
     "SL_1_RBT" => joinpath(dirname(@__DIR__), "src", "mappings", "S03SLSRBT_mapping.json"),
+    "SL_2_LST" => joinpath(dirname(@__DIR__), "src", "mappings", "S03SLSLST_mapping.json"),
     "SL_2_FRP" => joinpath(dirname(@__DIR__), "src", "mappings", "S03SLSFRP_mapping.json"),
 )
